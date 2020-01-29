0b94f2f0dcd6ed74c18b5bfbbc7e3458ec554ade
Shayoni Banerjee <Shayoni.Banerjee@ou.edu>
2020-01-29 13:14:15 -0600

    Did the coin toss particpation

---
+++ Participations/CoinToss/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, pick heads or tails >>");
+            string.usersGuessAsString = Console.ReadLine();
+            int usersGuess;
+
+            if (usersGuess == "heads") ;
+            {
+                usersGuess = 0;
+            }
+           else
+            {
+                usersGuess = 1;
+            }
+
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 2);
+
+            if (randomNumber = 0) ;
+            {
+                Console.WriteLine("The coin landed on heads");
+            }
+            if else (randomNumber = 1);
+            {
+                Console.WriteLine("The coin landed on tails");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
