59aafd887bd99a2d5bcb1521e296d183dfab5710
shayo <shayo@DESKTOP-OPA1O5H>
2020-02-19 13:07:50 -0600

    push 1

---
+++ Collections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examGrades = new List<double>();
+
+            do
+            {
+                Console.WriteLine("Enter an exam grade.");
+                double examGrade = Convert.ToDouble(Console.ReadKey());
+                examGrades.Add(examGrade);
+
+                Console.WriteLine("Do you want to enter another exam grade? Yes or no.");
+                String answer = Console.ReadLine();
+
+            }
+            while (answer == "yes");
+
+
+    }
+}
